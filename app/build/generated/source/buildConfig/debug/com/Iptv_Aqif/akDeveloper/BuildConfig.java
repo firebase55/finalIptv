827899cfdc712a066916be95c511df4b072236a1
DESKTOP-9P8OQF9 <firebase732@gmail.com>
2020-08-26 12:01:20 -0700

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/Iptv_Aqif/akDeveloper/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.Iptv_Aqif.akDeveloper;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.Iptv_Aqif.akDeveloper";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
